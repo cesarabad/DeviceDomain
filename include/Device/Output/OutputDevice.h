e6579cc07d31cc2d91f54a82bce87c98152d1a4e
cesarabad <cesarabadvel@gmail.com>
2026-01-18 13:32:54 +0100

    feat(Device, Lintener): Adding Device and Listener interfaces

---
+++ include/Device/Output/OutputDevice.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../Device.h"
+
+namespace Device {
+
+    template <typename T>
+    class OutputDevice : public virtual Device {
+    public:
+        OutputDevice(int device_serial_fd)
+            : Device(device_serial_fd) {
+        }
+
+        ~OutputDevice() override = default;
+
+        virtual void write(const T& data) const = 0;
+    };
+
+}
---
+++ include/Listener/DeviceListener.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <thread>
+#include <atomic>
+
+namespace Listener {
+
+    class DeviceListener {
+    public:
+        virtual ~DeviceListener() {
+            stop_listening();
+        }
+
+        virtual void listen() const = 0;
+
+        virtual void stop_listening() {
+            stopFlag_.store(true);
+            listening_ = false;
+            if (listenerThread_.joinable()) {
+                listenerThread_.join();
+            }
+        }
+
+    protected:
+        mutable std::thread listenerThread_;
+        mutable std::atomic<bool> stopFlag_{ false };
+        mutable bool listening_{ false };
+    };
+
+}
